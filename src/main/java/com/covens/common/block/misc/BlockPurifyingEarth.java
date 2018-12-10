d48c7d197c52b709e23d12db609d0258a421d361
ZaBi94 <zabi94@gmail.com>
2018-12-10 21:27:52 +0100

    Fix crash related to purifying earth

--- src/main/java/com/covens/common/block/misc/BlockPurifyingEarth.java
+++ src/main/java/com/covens/common/block/misc/BlockPurifyingEarth.java
@@ -1,11 +1,11 @@
 package com.covens.common.block.misc;
 
 import com.covens.api.CovensAPI;
-import com.covens.api.transformation.DefaultTransformations;
 import com.covens.api.transformation.ITransformation;
 import com.covens.common.block.BlockMod;
 import com.covens.common.content.transformation.CapabilityTransformation;
 import com.covens.common.lib.LibBlockName;
+
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
@@ -41,13 +41,19 @@ public class BlockPurifyingEarth extends BlockMod {
 	@Override
 	public void onEntityWalk(World worldIn, BlockPos pos, Entity entityIn) {
 		EnumCreatureAttribute attr = ((EntityLivingBase) entityIn).getCreatureAttribute();
-		ITransformation transformation = ((EntityPlayer) entityIn).getCapability(CapabilityTransformation.CAPABILITY, null).getType();
-		if (attr == EnumCreatureAttribute.UNDEAD || attr == CovensAPI.getAPI().DEMON || attr == CovensAPI.getAPI().SPIRIT || transformation == DefaultTransformations.VAMPIRE || transformation == DefaultTransformations.SPECTRE || transformation == DefaultTransformations.WEREWOLF) {
+		if (attr == EnumCreatureAttribute.UNDEAD || attr == CovensAPI.getAPI().DEMON || attr == CovensAPI.getAPI().SPIRIT || isTransformedPlayer(entityIn)) {
 			if (!entityIn.isBurning()) {
 				entityIn.setFire(1500);
 			}
-
 			super.onEntityWalk(worldIn, pos, entityIn);
 		}
+	}
+
+	private boolean isTransformedPlayer(Entity e) {
+		if (e instanceof EntityPlayer) {
+			ITransformation transformation = ((EntityPlayer) e).getCapability(CapabilityTransformation.CAPABILITY, null).getType();
+			return !transformation.canCrossSalt();
+		}
+		return false;
 	}
 }
