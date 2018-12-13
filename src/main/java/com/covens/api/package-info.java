94540a41b78d89e33f065b3daae0bfd12cd8309e
ZaBi94 <zabi94@gmail.com>
2018-12-13 19:10:24 +0100

    Version restart

--- src/main/java/com/covens/api/package-info.java
+++ src/main/java/com/covens/api/package-info.java
@@ -1,4 +1,4 @@
-@API(owner = com.covens.common.lib.LibMod.MOD_ID, apiVersion = "0.0.16", provides = "CovensAPI")
+@API(owner = com.covens.common.lib.LibMod.MOD_ID, apiVersion = "0", provides = "CovensAPI")
 package com.covens.api;
 
 import net.minecraftforge.fml.common.API;
