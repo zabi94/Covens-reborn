c8c2111fc9602545cfdc3ad0eab82702351505b3
ZaBi94 <zabi94@gmail.com>
2019-01-05 13:11:36 +0100

    Make familiars slightly translucent

--- src/main/java/com/covens/client/core/event/RenderingHacks.java
+++ src/main/java/com/covens/client/core/event/RenderingHacks.java
@@ -3,10 +3,15 @@ package com.covens.client.core.event;
 import java.util.HashMap;
 import java.util.UUID;
 
+import com.covens.api.CovensAPI;
+import com.covens.common.core.capability.familiar.CapabilityFamiliarCreature;
 import com.covens.common.item.block.ItemBlockRevealingLantern;
 
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.Tuple;
+import net.minecraftforge.client.event.RenderLivingEvent;
 import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -39,6 +44,22 @@ public class RenderingHacks {
 			Tuple<Float, Float> t = this.map.get(p.getUniqueID());
 			p.swingProgress = t.getFirst();
 			p.prevSwingProgress = t.getSecond();
+		}
+	}
+	
+	@SubscribeEvent
+	public void transparentFamiliars(RenderLivingEvent.Pre<EntityLivingBase> evt) {
+		if (CovensAPI.getAPI().isValidFamiliar(evt.getEntity()) && evt.getEntity().getCapability(CapabilityFamiliarCreature.CAPABILITY, null).hasOwner()) {
+			GlStateManager.enableBlend();
+			GlStateManager.color(0.7f, 0.7f, 1, 0.7f);
+		}
+	}
+	
+	@SubscribeEvent
+	public void transparentFamiliars(RenderLivingEvent.Post<EntityLivingBase> evt) {
+		if (CovensAPI.getAPI().isValidFamiliar(evt.getEntity()) && evt.getEntity().getCapability(CapabilityFamiliarCreature.CAPABILITY, null).hasOwner()) {
+			GlStateManager.disableBlend();
+			GlStateManager.color(1, 1, 1, 1f);
 		}
 	}
 
