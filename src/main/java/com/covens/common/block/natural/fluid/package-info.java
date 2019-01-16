b6a081e01bee48329d242afb919febe020fcf015
ZaBi94 <zabi94@gmail.com>
2019-01-16 18:44:33 +0100

    Get rid of useless classes

--- src/main/java/com/covens/common/block/natural/fluid/package-info.java
+++
@@ -1,7 +0,0 @@
-@MethodsReturnNonnullByDefault
-@ParametersAreNonnullByDefault
-package com.covens.common.block.natural.fluid;
-
-import javax.annotation.ParametersAreNonnullByDefault;
-
-import mcp.MethodsReturnNonnullByDefault;
