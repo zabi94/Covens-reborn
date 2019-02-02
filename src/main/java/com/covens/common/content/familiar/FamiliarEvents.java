9d90a384324db38585201a1209a8bbe48cca54ba
ZaBi94 <zabi94@gmail.com>
2019-02-02 16:53:32 +0100

    Make familiar AI better (READ DESCRIPTION)
    Needs a full dev-env rebuild (setupDecompWorkspace + eclipse/idea)

--- src/main/java/com/covens/common/content/familiar/FamiliarEvents.java
+++ src/main/java/com/covens/common/content/familiar/FamiliarEvents.java
@@ -1,5 +1,7 @@
 package com.covens.common.content.familiar;
 
+import java.util.Iterator;
+import java.util.Set;
 import java.util.UUID;
 
 import com.covens.api.CovensAPI;
@@ -13,10 +15,13 @@ import com.covens.common.content.familiar.ai.AIGotoPlace;
 import com.covens.common.core.capability.familiar.CapabilityFamiliarCreature;
 import com.covens.common.core.capability.familiar.CapabilityFamiliarOwner;
 import com.covens.common.core.util.syncTasks.FamiliarDeath;
+import com.google.common.collect.Sets;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.ai.EntityAIBase;
+import net.minecraft.entity.ai.EntityAITasks;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.math.RayTraceResult;
@@ -37,19 +42,24 @@ import zabi.minecraft.minerva.common.utils.entity.RayTraceHelper;
 @Mod.EventBusSubscriber
 public class FamiliarEvents {
 	
+	public static final Set<String> bannedTasks = Sets.newHashSet();
+	static {
+		bannedTasks.add("");
+	}
+
 	@SubscribeEvent(priority = EventPriority.LOWEST)
 	public static void familiarDeath(LivingDeathEvent evt) {
 		if (evt.getEntity().world.isRemote || !CovensAPI.getAPI().isValidFamiliar(evt.getEntity())) {
 			return;
 		}
-		CapabilityFamiliarCreature cap = evt.getEntity().getCapability(CapabilityFamiliarCreature.CAPABILITY, null); 
+		CapabilityFamiliarCreature cap = evt.getEntity().getCapability(CapabilityFamiliarCreature.CAPABILITY, null);
 		if (cap.hasOwner()) {
 			EntitySyncHelper.executeOnPlayerAvailable(cap.owner, new FamiliarDeath(evt.getEntity().getPersistentID(), evt.getEntity().getName()));
 			CovensAPI.getAPI().unbindFamiliar(evt.getEntity());
 		}
 		EntitySyncHelper.cleanMessagesForEntity(evt.getEntity().getPersistentID());
 	}
-	
+
 	@SubscribeEvent
 	public static void stopDespawn(LivingSpawnEvent.AllowDespawn evt) {
 		if (!CovensAPI.getAPI().isValidFamiliar(evt.getEntity())) {
@@ -59,30 +69,65 @@ public class FamiliarEvents {
 			evt.setResult(Result.DENY);
 		}
 	}
-	
+
 	@SubscribeEvent
 	public static void attachFamiliarAI(EntityJoinWorldEvent evt) {
-		//Don't use the capabilities at this point, they are still not attached
-		if (!evt.getWorld().isRemote && evt.getEntity() instanceof EntityLiving && CovensAPI.getAPI().isValidFamiliar(evt.getEntity())) {
+		if (!evt.getWorld().isRemote && (evt.getEntity() instanceof EntityLiving) && CovensAPI.getAPI().isValidFamiliar(evt.getEntity())) {
 			EntityLiving entity = (EntityLiving) evt.getEntity();
-			entity.tasks.addTask(2, new AIFollowTarget(entity));
-			entity.tasks.addTask(3, new AIGotoPlace(entity));
-			entity.tasks.addTask(4, new AIFamiliarSit(entity));
+			if (entity.getCapability(CapabilityFamiliarCreature.CAPABILITY, null).hasOwner()) {
+				removeTasks(entity);
+				removeTargets(entity);
+				entity.tasks.addTask(2, new AIFollowTarget(entity));
+				entity.tasks.addTask(3, new AIGotoPlace(entity));
+				entity.tasks.addTask(4, new AIFamiliarSit(entity));
+//				entity.targetTasks.addTask(1, new EntityAIOwnerHurtByTarget(entity));
+//				entity.targetTasks.addTask(2, new EntityAIOwnerHurtTarget(entity));
+			}
 		}
 	}
-	
+
+	private static void removeTasks(EntityLiving elb) {
+		Iterator<EntityAITasks.EntityAITaskEntry> iterator = elb.tasks.taskEntries.iterator();
+		while (iterator.hasNext()) {
+			EntityAITasks.EntityAITaskEntry entityaitasks$entityaitaskentry = iterator.next();
+			EntityAIBase entityaibase = entityaitasks$entityaitaskentry.action;
+			if (bannedTasks.contains(entityaibase.getClass().getName())) {
+				if (entityaitasks$entityaitaskentry.using) {
+					entityaitasks$entityaitaskentry.using = false;
+					entityaitasks$entityaitaskentry.action.resetTask();
+					elb.tasks.executingTaskEntries.remove(entityaitasks$entityaitaskentry);
+				}
+				iterator.remove();
+				return;
+			}
+		}
+	}
+
+	private static void removeTargets(EntityLiving elb) {
+		Iterator<EntityAITasks.EntityAITaskEntry> iterator = elb.targetTasks.taskEntries.iterator();
+		while (iterator.hasNext()) {
+			EntityAITasks.EntityAITaskEntry entry = iterator.next();
+			if (entry.using) {
+				entry.using = false;
+				entry.action.resetTask();
+				elb.targetTasks.executingTaskEntries.remove(entry);
+			}
+			iterator.remove();
+		}
+	}
+
 	@SubscribeEvent
 	public static void addActions(HotbarActionCollectionEvent evt) {
 		if (evt.player.getCapability(CapabilityFamiliarOwner.CAPABILITY, null).familiarCount > 0) {
 			evt.getList().add(ModAbilities.COMMAND_FAMILIAR);
 		}
 	}
-	
+
 	@SubscribeEvent
 	public static void handleCommandAbility(HotbarActionTriggeredEvent evt) {
 		if (evt.action.equals(ModAbilities.COMMAND_FAMILIAR)) {
 			Entity e = evt.focusedEntity;
-			if (e != null && CovensAPI.getAPI().isValidFamiliar(e) && e.getCapability(CapabilityFamiliarCreature.CAPABILITY, null).owner.equals(evt.player.getUniqueID())) {
+			if ((e != null) && CovensAPI.getAPI().isValidFamiliar(e) && e.getCapability(CapabilityFamiliarCreature.CAPABILITY, null).owner.equals(evt.player.getUniqueID())) {
 				handleClickOnFamiliar(evt.player, (EntityLiving) e);
 			} else {
 				RayTraceResult result = RayTraceHelper.rayTracePlayerSight(evt.player, 32, true);
@@ -106,7 +151,7 @@ public class FamiliarEvents {
 			}
 		}
 	}
-	
+
 	@SubscribeEvent
 	public static void onPlayerLogin(PlayerLoggedInEvent evt) {
 		if (evt.player instanceof EntityPlayerMP) {
