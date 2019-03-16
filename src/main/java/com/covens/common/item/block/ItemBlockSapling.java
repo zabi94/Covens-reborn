4615848a2f3010464f7f731657fa9461c5972706
ZaBi94 <zabi94@gmail.com>
2019-03-16 14:53:44 +0100

    Explicitly declare sapling suptypes

--- src/main/java/com/covens/common/item/block/ItemBlockSapling.java
+++ src/main/java/com/covens/common/item/block/ItemBlockSapling.java
@@ -3,10 +3,13 @@
 package com.covens.common.item.block;
 
 import com.covens.common.block.ModBlocks;
+import com.covens.common.block.natural.tree.BlockModSapling;
 import com.covens.common.block.natural.tree.BlockModSapling.EnumSaplingType;
 import com.covens.common.item.ItemBlockMod;
 
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
 
 public class ItemBlockSapling extends ItemBlockMod {
 
@@ -31,6 +34,15 @@ public class ItemBlockSapling extends ItemBlockMod {
 	@Override
 	public boolean getHasSubtypes() {
 		return true;
+	}
+	
+	@Override
+	public void getSubItems(CreativeTabs tab, NonNullList<ItemStack> items) {
+		if (this.isInCreativeTab(tab)) {
+			for (BlockModSapling.EnumSaplingType t:BlockModSapling.EnumSaplingType.values()) {
+				items.add(new ItemStack(this, 1, t.ordinal()));
+			}
+		}
 	}
 
 	@Override
