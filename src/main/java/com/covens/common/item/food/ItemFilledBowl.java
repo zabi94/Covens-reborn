58733d54c63d695ec95397ef969f3b22fd2538d4
ZaBi94 <zabi94@gmail.com>
2019-04-01 12:28:52 +0100

    Make the default stew item not empty

--- src/main/java/com/covens/common/item/food/ItemFilledBowl.java
+++ src/main/java/com/covens/common/item/food/ItemFilledBowl.java
@@ -10,23 +10,38 @@ import com.covens.common.lib.LibItemName;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.FoodStats;
+import net.minecraft.util.NonNullList;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.registry.GameRegistry.ItemStackHolder;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ItemFilledBowl extends ItemModFood {
+	
+	
+	@ItemStackHolder(value = "covens:stew", nbt = "{hunger:5,saturation:5.0f}")
+	public static final ItemStack stack = null;
 
 	public ItemFilledBowl() {
 		super(LibItemName.FILLED_BOWL, 0, 0f, true);
 		this.setCreativeTab(ModCreativeTabs.ITEMS_CREATIVE_TAB);
 		this.setMaxStackSize(16);
+		this.setHasSubtypes(true);
 	}
-
+	
+	@Override
+	public void getSubItems(CreativeTabs tab, NonNullList<ItemStack> items) {
+		if (this.isInCreativeTab(tab)) {
+			items.add(stack.copy());
+		}
+	}
+	
 	@Override
 	public void onFoodEaten(ItemStack stack, World worldIn, EntityPlayer player) {
 		super.onFoodEaten(stack, worldIn, player);
