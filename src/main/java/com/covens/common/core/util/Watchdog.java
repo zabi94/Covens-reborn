9aa4bce25a444dfc2dbd6144bd05a8b1e2620bb2
Simone Zanin <zabi94@gmail.com>
2019-06-23 18:06:10 +0200

    Drop watchdog

--- src/main/java/com/covens/common/core/util/Watchdog.java
+++ src/main/java/com/covens/common/core/util/Watchdog.java
@@ -1,15 +1,12 @@
 package com.covens.common.core.util;
 
-import net.minecraftforge.fml.common.Loader;
-import net.minecraftforge.fml.common.LoaderException;
-
 public class Watchdog {
 	public static void init() {
 		// OpenEye uses non lowercase modid, I can't put it inside the standard deps
-		if (!"true".equals(System.getProperty("ignoreMissingOpenEye")) && !Loader.isModLoaded("OpenEye")) {
-			LoaderException l = new LoaderException("\n\n\n\n\nMod OpenEye is required for alpha testing of Covens Reborn\n\n\n\n\n");
-			l.setStackTrace(new StackTraceElement[0]);
-			throw l;
-		}
+//		if (!"true".equals(System.getProperty("ignoreMissingOpenEye")) && !Loader.isModLoaded("OpenEye")) {
+//			LoaderException l = new LoaderException("\n\n\n\n\nMod OpenEye is required for alpha testing of Covens Reborn\n\n\n\n\n");
+//			l.setStackTrace(new StackTraceElement[0]);
+//			throw l;
+//		}
 	}
 }
